47a23349860e131874cab56037a8739b47ac5bb0
MatheusAraujo110 <theusnascimento386@gmail.com>
2024-05-04 02:00:03 -0300

    responsivo

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const form = document.querySelector(".formulario-fale-conosco")
 const mascara = document.querySelector(".mascara-formulario")
 
 function mostrarForm(){
-    form.style.left = "50%"
+    form.style.left = "12%"
     form.style.transform = "translateX(-50%);"
     mascara.style.visibility = "visible"
 }
